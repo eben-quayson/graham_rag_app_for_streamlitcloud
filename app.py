c14900509eb5cb48cf1cef62d8eb3ada6c77da75
Ebenezer Quayson <quaysonebenezerawudzi@gmail.com>
2025-02-12 05:53:28 +0000

    update sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import pysqlite3
+import sys
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+
+
+
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
